48e46e3f33239f709da30b6259fc987605636064
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2026-03-16 20:56:25 -0400

    Format event date and time for display

--- client/src/components/Event.jsx
+++ client/src/components/Event.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import '../css/Event.css'
 
 const Event = (props) => {
+
+    const formattedDate = new Date(props.date).toLocaleDateString()
+
+    const formattedTime = new Date(`1970-01-01T${props.time}`).toLocaleTimeString([], {
+        hour: 'numeric',
+        minute: '2-digit'
+    })
+
     return (
         <article className='event-information'>
             <img src={props.image} />
@@ -10,9 +18,9 @@ const Event = (props) => {
                 <div className='text'>
                     <h3>{props.title}</h3>
                     <p>
-                        <i className="fa-regular fa-calendar fa-bounce"></i> {props.date}
+                        <i className="fa-regular fa-calendar fa-bounce"></i> {formattedDate}
                         <br />
-                        {props.time}
+                        {formattedTime}
                     </p>
                 </div>
             </div>
